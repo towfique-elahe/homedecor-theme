635006301177652e7cfe225d8dd4c9f9dc93730d
towfique-elahe <towfiqueelahe6@gmail.com>
2024-10-12 14:23:31 +0600

    Update: Version 1.8

--- template-parts/transform-home-section.php
+++ template-parts/transform-home-section.php
@@ -3,7 +3,7 @@
         <div class="row">
             <!-- Left Side: Title, Description, CTA Button -->
             <div class="col branding-content">
-                <h2 class="branding-title section_heading">Transform Your Home with Home Decor</h2>
+                <h3 class="branding-title section_heading">Transform Your Home with <?php bloginfo('name'); ?></h3>
                 <p class="branding-description">Discover the latest trends in home decor and elevate your living space with our exclusive collection. Whether you're looking for modern, classic, or eclectic styles, we have something to suit every taste. Let's turn your house into a home you'll love.</p>
                 <a href="<?php echo esc_url(home_url('/index.php/shop')); ?>" class="cta-button">Shop Now</a>
             </div>
